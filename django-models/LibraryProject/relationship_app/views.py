136f3a01a58828b66a81d19ce72b4359785a9198
Fridah Nthambi <162557127+Fridah34@users.noreply.github.com>
2025-09-08 17:38:30 +0300

    implemented views and urls configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,21 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 from .models import Book, Library
-from django.http import HttpResponse
 
 
 # --- Function-based View ---
 def list_books(request):
     books = Book.objects.all()
-     # ✅ Plain text output (for checker)
-    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(output if output else "No books available.")
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
 # --- Class-based View ---
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
 
     # override get_object to lookup by ID or name if needed

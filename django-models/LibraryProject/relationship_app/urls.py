00d290eee6e4db5616aedb860c40072c63d81b64
Fridah Nthambi <162557127+Fridah34@users.noreply.github.com>
2025-09-09 00:06:02 +0300

    implemented customs permissions in django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
     path("member-dashboard/", views.member_view, name="member_view"),
     
     path('books/add/', views.add_book, name='add_book'),
-    path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
-    path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
+    path('books/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('books/<int:book_id>/', views.delete_book, name='delete_book'),
 ]

c81031c9f5ae214a6fc7c2aac065817f1cb510ea
Fridah Nthambi <162557127+Fridah34@users.noreply.github.com>
2025-09-08 17:17:04 +0300

    implemented views and urls configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+
+# --- Function-based View ---
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "list_books.html", {"books": books})
+
+
+# --- Class-based View ---
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = "library"
+
+    # override get_object to lookup by ID or name if needed
+    def get_object(self, queryset=None):
+        return get_object_or_404(Library, pk=self.kwargs.get("pk"))
+

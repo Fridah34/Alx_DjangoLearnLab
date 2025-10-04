dd76322f01ee8769340bde25db6afc5d4f4f719d
Fridah Nthambi <162557127+Fridah34@users.noreply.github.com>
2025-10-04 23:34:04 +0300

    implemented custom views and generic views in django rest framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import viewsets, generics, permissions
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Author, Book
 from .serializers import AuthorSerializer, BookSerializer
 
@@ -16,32 +17,32 @@ class BookViewSet(viewsets.ModelViewSet):
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]  # anyone can view list
+    permission_classes = [IsAuthenticatedOrReadOnly]  # anyone can view list
 
 
 # DetailView – Retrieve a single book by ID
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]  # anyone can view details
+    permission_classes = [IsAuthenticatedOrReadOnly]  # anyone can view details
 
 
 # CreateView – Add a new book
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]  # only logged in users can create
+    permission_classes = [IsAuthenticated]  # only logged in users can create
 
 
 # UpdateView – Modify an existing book
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]  # only logged in users can update
+    permission_classes = [IsAuthenticated]  # only logged in users can update
 
 
 # DeleteView – Remove a book
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]  # only logged in users can delete
+    permission_classes = [IsAuthenticated]  # only logged in users can delete

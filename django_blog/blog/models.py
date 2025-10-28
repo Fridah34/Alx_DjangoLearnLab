7504e76c884e3c3c2fecbf6dd655cccffff1ca31
Fridah Nthambi <162557127+Fridah34@users.noreply.github.com>
2025-10-28 23:09:16 +0300

    fixed the auth models error

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,18 +1,13 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User 
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='posts'
-    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    class Meta:
-        ordering = ['-published_date']
 
     def __str__(self):
         return self.title

63de89e3909774d0246e63fa26b5bc2839282df1
Fridah Nthambi <162557127+Fridah34@users.noreply.github.com>
2025-10-28 23:34:04 +0300

    Implemented blog post CRUD (List, Detail, Create, Update, Delete) with permissions

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -67,7 +67,7 @@ def profile_view(request):
 # List all posts
 class PostListView(ListView):
     model = Post
-    template_name = 'post_list.html'
+    template_name = 'blog/post_list.html'
     context_object_name = 'posts'
     ordering = ['-published_date']
 
@@ -75,14 +75,14 @@ class PostListView(ListView):
 # View post details
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'post_detail.html'
+    template_name = 'blog/post_detail.html'
 
 
 # Create new post
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
-    template_name = 'post_form.html'
+    template_name = 'blog/post_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -93,7 +93,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
-    template_name = 'post_form.html'
+    template_name = 'blog/post_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
@@ -107,7 +107,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 # Delete post
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
-    template_name = 'post_confirm_delete.html'
+    template_name = 'blog/post_confirm_delete.html'
     success_url = reverse_lazy('post-list')
 
     def test_func(self):
